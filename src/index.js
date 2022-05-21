b7a7269a2ec9523983e62e7299753c52fb3127d0
DenisTolmachev <denis.tolmachev@gmail.com>
2022-05-21 22:21:53 +0300

    add fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './sass/main.scss';
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import galleryCard from './templates/galleryCard'
+import { fetchGallery } from './js/fetchGallery';
+
+const searchForm = document.querySelector('#search-form');
+
+function inputHandler(e) {
+   const inputValue = e.target.value;
+   console.log(inputValue);
+}
+
+searchForm.addEventListener('input', inputHandler)
+
+
+console.log(fetchGallery('cat'));
---
+++ src/js/fetchGallery.js
@@ -0,0 +1,16 @@
+
+
+export function fetchGallery(name) {
+  const url = 'https://pixabay.com/api/';
+  const PERSONAL_KEY = '27539688-ef1c1b76d02735ef30548b858';
+
+  return fetch(
+    `${url}?key=${PERSONAL_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`,
+  )
+    .then(r => {
+      if (r.ok) {
+        return r.json();
+      }
+      throw new Error(r.status);
+    });
+}
