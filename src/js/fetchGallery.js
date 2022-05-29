055ebcf262f6a0048a03d072953396df7b4d3bab
Denis <denis.tolmachev@gmail.com>
2022-05-29 12:54:23 +0300

    finaly correct

--- src/js/fetchGallery.js
+++ src/js/fetchGallery.js
@@ -13,10 +13,10 @@ export const queryOptions = {
   page: 1,
 };
 
-export const fetchGallery = async params => {
+export const fetchGallery = async options => {
   const url = `${BASE_URL}?key=${API_KEY}`;
   try {
-    const result = await axios.get(url, { params });
+    const result = await axios.get(url, { options });
     return result;
   } catch {
     Notiflix.Notify.failure('Sorry, its data error');
